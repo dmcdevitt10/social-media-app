811b7b4cac9edb71d3d5080f35b79c2c37631301
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-05-02 17:53:30 -0600

    server is set up with controller functions and endpoints

---
+++ server/controllers/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+  login: (req, res) => {
+    console.log("login");
+  },
+  register: (req, res) => {
+    console.log('register')
+  },
+  logout: (req, res) => {
+    console.log("logout");
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const {PORT} = process.env
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const {login, register, logout} = require('./controllers/auth')
+const {getAllPosts, getCurrentUserPosts, addPost, editPost, deletePost} = require('./controllers/posts')
+const {isAuthenticated} = require('./middleware/isAuthenticated')
+
+
+app.get('/posts', getAllPosts)
+app.get('./userposts/:userId', getCurrentUserPosts)
+
+app.post('/register', register)
+app.post('/login', login)
+app.post('/posts', isAuthenticated, addPost)
+
+app.put('/posts/:id', isAuthenticated, editPost)
+
+app.delete('/posts/:id', isAuthenticated, deletePost)
+
+
+app.listen(PORT, () => console.log(`app is listening on port ${PORT}`))
